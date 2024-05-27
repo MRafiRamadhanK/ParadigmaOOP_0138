de57e75cdbdca8e94ae0be7362c27f2bc63688f7
MRafiramadhanK <raficool271003@gmail.com>
2024-05-27 10:05:32 +0700

    int main

--- ParadigmaOOP_0138/ParadigmaOOP_0138.cpp
+++ ParadigmaOOP_0138/ParadigmaOOP_0138.cpp
@@ -11,3 +11,23 @@ public:
 		cout << "pesan dari joko" << endl;
 	}
 };
+
+class lia :public seseorang {
+public:
+	void pesan() {
+		cout << "pesan dari lia" << endl;
+	}
+};
+
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+
+	return 0;
+}

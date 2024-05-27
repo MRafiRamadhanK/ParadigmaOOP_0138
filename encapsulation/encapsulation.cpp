3ce1fce7f3a5f1c893be78b847ed7d4ff1e5fdc2
MRafiramadhanK <raficool271003@gmail.com>
2024-05-27 10:16:43 +0700

    int main

--- encapsulation/encapsulation.cpp
+++ encapsulation/encapsulation.cpp
@@ -10,6 +10,17 @@ public :
 		saklarNo[i] = value;
 	}
 	string getSaklarNo(int i) {
-		return saklarNo[i]
+		return saklarNo[i];
 	}
 };
+
+int main() {
+	remoteLampu lampuRumah;
+
+	lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+	lampuRumah.setSaklarNo(2, "Lampu Kamar tidur");
+	lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+	cout << lampuRumah.getSaklarNo(0) << endl;
+}
